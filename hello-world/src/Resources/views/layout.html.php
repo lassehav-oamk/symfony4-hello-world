f63ffcacca481066f741bc0f72fc2d66e783e9e0
Lasse Haverinen <lasse.haverinen@gmail.com>
2018-08-13 11:37:09 +0300

    Added CSS asset example

--- hello-world/src/Resources/views/layout.html.php
+++ hello-world/src/Resources/views/layout.html.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <link href="<?php echo $view['assets']->getUrl('css/global.css') ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <?php $view['slots']->output('_content') ?>
